a6cfa6fe1f5abe6357f2c9712666de91a9b3f362
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-04-18 11:53:22 -0300

    inicializando projeto spring

---
+++ src/main/java/com/neobank/neobankapi/NeobankapiApplication.java
@@ -0,0 +1,13 @@
+package com.neobank.neobankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeobankapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeobankapiApplication.class, args);
+	}
+
+}
