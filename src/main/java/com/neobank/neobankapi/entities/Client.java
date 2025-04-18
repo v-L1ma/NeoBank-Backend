d373e3e6aaced87fe9db00389d9a2d1df7034fc6
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-04-18 12:43:59 -0300

    criando as entities

---
+++ src/main/java/com/neobank/neobankapi/entities/Client.java
@@ -0,0 +1,85 @@
+package com.neobank.neobankapi.entities;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.UUID)
+    @Column(name = "client_id")
+    private UUID clientId;
+
+    private String name;
+
+    @Column(unique=true)
+    private String email;
+
+    private String password;
+
+    private Double balance;
+
+    @CreationTimestamp
+    private Instant creationTimeStamp;
+
+    public UUID getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(UUID clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Instant getCreationTimeStamp() {
+        return creationTimeStamp;
+    }
+
+    public void setCreationTimeStamp(Instant creationTimeStamp) {
+        this.creationTimeStamp = creationTimeStamp;
+    }
+
+
+}
---
+++ src/main/java/com/neobank/neobankapi/entities/Transaction.java
@@ -0,0 +1,64 @@
+package com.neobank.neobankapi.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "tb_transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    @Column(name="transaction_id")
+    private Long transactionId;
+
+
+    @ManyToOne
+    @JoinColumn(name= "sender_id", referencedColumnName="client_id")
+    private Client sender;
+
+    @ManyToOne
+    @JoinColumn(name= "receiver_id", referencedColumnName="client_id")
+    private Client receiver;
+
+    private Double value;
+
+    public Long gettransactionId() {
+        return transactionId;
+    }
+
+    public void settransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Client getSender() {
+        return sender;
+    }
+
+    public void setSender(Client sender) {
+        this.sender = sender;
+    }
+
+    public Client getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Client receiver) {
+        this.receiver = receiver;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+}
